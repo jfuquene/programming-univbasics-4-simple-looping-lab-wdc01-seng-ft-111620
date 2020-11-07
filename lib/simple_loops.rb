ed9234374274defbf94a7f9b543af183ea752d81
John Fuquene <jfuquene@email.wm.edu>
2020-11-07 03:38:21 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -17,11 +17,20 @@ count = 0
  end 
 
  
- def output_array (array)
+ def output_array(array)
    count = 0 
    
-   while array[count] do 
+   while count < array.length do 
      puts array[count]
+     count += 1 
+   end 
+ end 
+ 
+ def return_string_array (array)
+   count = 0 
+   
+   while count < array.length do 
+     puts array[count].to_s 
      count += 1 
    end 
  end 
